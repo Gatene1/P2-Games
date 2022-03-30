9421ab3454d99a151c99cc6d7caf597f4829f6c1
TannerHartt <tannerhart24@yahoo.com>
2022-03-30 12:00:08 -0700

    refactoring to include spring backend tool

---
+++ backend/src/main/java/com/revature/p2Group1/P2Group1Application.java
@@ -0,0 +1,13 @@
+package com.revature.p2Group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2Group1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2Group1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/revature/p2Group1/P2Group1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.p2Group1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2Group1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
